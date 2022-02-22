287f0be12308e8997a46367d6d6f4601c732f88e
Nik Volkov <vistusx93@gmail.com>
2022-02-22 20:18:59 +0300

    Fix alghoritm

--- src/Program.cs
+++ src/Program.cs
@@ -120,10 +120,10 @@ namespace PicUnlocker
             ///                 X      7/16
             ///     3 / 16   5 / 16   1 / 16
 
-            double FirstErrorWeight = 7 / 16;
-            double SecondErrorWeight = 3 / 16;
-            double ThirdErrorWeight = 5 / 16;
-            double LastErrorWeight = 1 / 16;
+            double FirstErrorWeight = 7.0 / 16.0;
+            double SecondErrorWeight = 3.0 / 16.0;
+            double ThirdErrorWeight = 5.0 / 16.0;
+            double LastErrorWeight = 1.0 / 16.0;
 
             var pixelsImage = new Bitmap(Image.FromFile(filePath));
             var newImage = new Bitmap(pixelsImage.Width, pixelsImage.Height);
@@ -134,52 +134,69 @@ namespace PicUnlocker
                 {
 
                     var currentPixel = pixelsImage.GetPixel(x, y);
-                    {
-                        var newColors = CalculateChannelValueWithError(currentPixel.R, currentPixel.G, currentPixel.B, paletteBitSize, FirstErrorWeight);
-                        Color newPixelData = Color.FromArgb(newColors.R, newColors.G, newColors.B);
-                        newImage.SetPixel(x + 1, y, newPixelData);
-                    }
-                    {
-                        var newColors = CalculateChannelValueWithError(currentPixel.R, currentPixel.G, currentPixel.B, paletteBitSize, SecondErrorWeight);
-                        var newPixelData = Color.FromArgb(newColors.R, newColors.G, newColors.B);
-                        newImage.SetPixel(x - 1, y + 1, newPixelData);
-                    }
-                    
-                    {
-                        var newColors = CalculateChannelValueWithError(currentPixel.R, currentPixel.G, currentPixel.B, paletteBitSize, ThirdErrorWeight);
-                        var newPixelData = Color.FromArgb(newColors.R, newColors.G, newColors.B);
-                        newImage.SetPixel(x, y + 1, newPixelData);
-                    }
-                    
-                    {
-                        var newColors = CalculateChannelValueWithError(currentPixel.R, currentPixel.G, currentPixel.B, paletteBitSize, LastErrorWeight);
-                        var newPixelData = Color.FromArgb(newColors.R, newColors.G, newColors.B);
-                        newImage.SetPixel(x + 1, y + 1, newPixelData);
-                    }
-                    
+                    var newR = CalculateQuantitation(currentPixel.R, paletteBitSize);
+                    var newG = CalculateQuantitation(currentPixel.G, paletteBitSize);
+                    var newB = CalculateQuantitation(currentPixel.B, paletteBitSize);
+                    Color newPixelData = Color.FromArgb(newR, newG, newB);
+                    newImage.SetPixel(x, y, newPixelData);
 
+                    var currentErrors = CalculateError(currentPixel.R, currentPixel.G, currentPixel.B, paletteBitSize);
+                    
+                    var nextPixel = pixelsImage.GetPixel(x + 1, y);
+                    newR = CalculateColorWithError(nextPixel.R, currentErrors.R, FirstErrorWeight);
+                    newG = CalculateColorWithError(nextPixel.G, currentErrors.G, FirstErrorWeight);
+                    newB = CalculateColorWithError(nextPixel.B, currentErrors.B, FirstErrorWeight);
+                    newPixelData = Color.FromArgb(newR, newG, newB);
+                    newImage.SetPixel(x + 1, y, newPixelData);
+
+                    nextPixel = pixelsImage.GetPixel(x + 1, y);
+                    newR = CalculateColorWithError(nextPixel.R, currentErrors.R, SecondErrorWeight);
+                    newG = CalculateColorWithError(nextPixel.G, currentErrors.G, SecondErrorWeight);
+                    newB = CalculateColorWithError(nextPixel.B, currentErrors.B, SecondErrorWeight);
+                    newPixelData = Color.FromArgb(newR, newG, newB);
+                    newImage.SetPixel(x - 1, y + 1, newPixelData);
+
+                    nextPixel = pixelsImage.GetPixel(x + 1, y);
+                    newR = CalculateColorWithError(nextPixel.R, currentErrors.R, ThirdErrorWeight);
+                    newG = CalculateColorWithError(nextPixel.G, currentErrors.G, ThirdErrorWeight);
+                    newB = CalculateColorWithError(nextPixel.B, currentErrors.B, ThirdErrorWeight);
+                    newPixelData = Color.FromArgb(newR, newG, newB);
+                    newImage.SetPixel(x, y + 1, newPixelData);
+
+                    nextPixel = pixelsImage.GetPixel(x + 1, y);
+                    newR = CalculateColorWithError(nextPixel.R, currentErrors.R, LastErrorWeight);
+                    newG = CalculateColorWithError(nextPixel.G, currentErrors.G, LastErrorWeight);
+                    newB = CalculateColorWithError(nextPixel.B, currentErrors.B, LastErrorWeight);
+                    newPixelData = Color.FromArgb(newR, newG, newB);
+                    newImage.SetPixel(x + 1, y + 1, newPixelData);
                 }
             }
             newImage.Save("test_new.jpg");
             Console.WriteLine("Done");
         }
 
-        private static (int R, int G, int B) CalculateChannelValueWithError(int channelRValue, int channelGValue, int channelBValue, int paletteBitSize, double errorWeight)
+        private static (int R, int G, int B) CalculateError(int channelRValue, int channelGValue, int channelBValue, int paletteBitSize)
         {
-            var channelRError = channelRValue - CalculateErrorOnChannel(channelRValue, paletteBitSize);
-            var updatedChannelRValue = channelRValue + channelRError * errorWeight; 
+            var channelRError = channelRValue - CalculateQuantitation(channelRValue, paletteBitSize);
+            var updatedChannelRValue = channelRValue + channelRError; 
 
-            var channelGError = channelGValue - CalculateErrorOnChannel(channelGValue, paletteBitSize);
-            var updatedChannelGValue = channelGValue + channelGError * errorWeight; 
+            var channelGError = channelGValue - CalculateQuantitation(channelGValue, paletteBitSize);
+            var updatedChannelGValue = channelGValue + channelGError; 
 
-            var channelBError = channelBValue - CalculateErrorOnChannel(channelBValue, paletteBitSize);
-            var updatedChannelBValue = channelBValue + channelBError * errorWeight;
+            var channelBError = channelBValue - CalculateQuantitation(channelBValue, paletteBitSize);
+            var updatedChannelBValue = channelBValue + channelBError;
 
-           // Console.WriteLine($"R: {(int)updatedChannelRValue}, G: {(int)updatedChannelGValue}, B: {(int)updatedChannelBValue}");
             return (R: (int)updatedChannelRValue, G: (int)updatedChannelGValue, B: (int)updatedChannelBValue);
         }
 
-        private static int CalculateErrorOnChannel(int channelValue, int paletteBitSize)
+        private static int CalculateColorWithError(int channelValue, int channelError, double errorWeight)
+        {
+            var updatedChannelValue = channelValue + channelError * errorWeight;
+
+            return (int)updatedChannelValue;
+        }
+
+        private static int CalculateQuantitation(int channelValue, int paletteBitSize)
         {
             return (int)(Math.Round(Convert.ToDouble(paletteBitSize * channelValue / 255)) * 255 / paletteBitSize);
         }
